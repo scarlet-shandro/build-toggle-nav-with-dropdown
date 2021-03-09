f90d8d3c94bf49702e18982e4d4133499896e39e
Scarlet Shandro <scarletms@live.ca>
2021-03-08 18:34:49 -0700

    page 1 done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-// js goes here
+const toggle = document.querySelector('.menu-icon');
+
+toggle.addEventListener('click', () => {
+    
+    document.querySelector('nav').classList.toggle('show-nav');
+});
+
